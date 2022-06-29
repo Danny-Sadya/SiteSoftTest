49566e3d7a2c6ae833130da723cbb8e789dbebb3
Danny-Sadya <77333042+Danny-Sadya@users.noreply.github.com>
2022-06-29 13:26:31 +0500

    Update admin.py
    Added scraped_date field to Django Admin

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(PostModel)
+# admin.site.register(PostModel)
+@admin.register(PostModel)
+class PostModelAdmin(admin.ModelAdmin):
+    readonly_fields = ('scraped_date',)
+
+
 # admin.site.register(PostTagModel)
+
 admin.site.register(PostToScrapeModel)
